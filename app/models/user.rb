10c06e4902a99a2579e8c34562ce6378372be04e
Jesse <nguyenjessev@gmail.com>
2021-01-16 15:53:26 -1000

    Add User/Post association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :username, presence: true, length: { minimum: 2 }, uniqueness: true
   validates :email, presence: true
   validates :password, presence: true, length: {minimum: 8 }
+
+  has_many :posts
 end
